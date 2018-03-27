e8ff893b572d0d06232c1e4edd7ff5732b89a7f2
Juliette Simard <jsimard619@gmail.com>
2018-03-27 13:11:16 -0400

    add dog file

---
+++ dog.py
@@ -0,0 +1,23 @@
+class Dog:
+    """This is the beggining of a class humble house dog"""
+    
+d = Dog()
+x = Dog()
+
+d.name = "Tyson"
+
+print(d.name)
+d.weight = 100
+d.breed = "Black Lab,German Sheppard, Rottweiler, and Boxer"
+
+print(d.weight)
+print(d.breed)
+
+x.name = "Layla"
+
+print(x.name)
+x.weight = 90
+x.breed = "Pure Breed Black Lab"
+
+print(x.weight)
+print(x.breed)
